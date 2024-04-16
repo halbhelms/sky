78c8017be849e924110778fdd43e68753d3d473f
Hal Helms <hal.helms@gmail.com>
2024-04-16 10:25:26 -0400

    Add section-title

---
+++ packages/skyui/src/lib/components/ui/section-title.svelte
@@ -0,0 +1,29 @@
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+
+<!-- #region TS -->
+<script lang="ts">
+  export let title: string
+</script>
+<!-- #endregion -->
+
+
+<!-- #region HTML -->
+<section class="filename-component">
+  <h1>{title}</h1>
+</section>
+<!-- #endregion -->
+
+
+<!-- #region CSS -->
+<style>
+  .filename-component {
+  }
+
+  h1 {
+    color: hsl(225,96%,64%);
+    text-align: center;
+    margin: 1.25rem 0 0.5rem 0;
+  }
+</style>
+<!-- #endregion -->
